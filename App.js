5b218baed40e17aaa1403765b3d50b70bb9d091c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-16 18:07:14 -0800

    Adds and annotation example to be rendered in mapview

--- App.js
+++ App.js
@@ -3,27 +3,38 @@ import {View, StyleSheet, Text, Image} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import {REACT_APP_TEST_ENV, REACT_APP_ACCESS_KEY} from '@env';
 
-// MapboxGL.setAccessToken(REACT_APP_ACCESS_KEY);
+MapboxGL.setAccessToken(REACT_APP_ACCESS_KEY);
 
 const App = () => {
   const [coordinates] = useState([8.674252499999994, 9.0845755]);
+  const renderAnnotations = () => {
+    return (
+      <MapboxGL.PointAnnotation
+        key="pointAnnotation"
+        id="pointAnnotation"
+        coordinate={[8.51672, 12.00012]}>
+        <View
+          style={{
+            height: 30,
+            width: 30,
+            backgroundColor: 'black',
+            borderRadius: 50,
+            borderColor: 'black',
+            borderWidth: 3,
+          }}
+        />
+      </MapboxGL.PointAnnotation>
+    );
+  };
   return (
-    // <View style={styles.page}>
-    //   <View style={styles.container}>
-    //     <MapboxGL.MapView style={styles.map}>
-    //       <MapboxGL.Camera zoomLevel={6} centerCoordinate={coordinates} />
-    //       <MapboxGL.PointAnnotation coordinate={coordinates} />
-    //     </MapboxGL.MapView>
-    //   </View>
-    // </View>
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Test ENV: {REACT_APP_TEST_ENV} </Text>
-      <Text>Test ENV: {REACT_APP_ACCESS_KEY} </Text>
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapboxGL.MapView style={styles.map}>
+          <MapboxGL.Camera zoomLevel={6} centerCoordinate={coordinates} />
+          <MapboxGL.PointAnnotation coordinate={coordinates} />
+          <>{renderAnnotations()}</>
+        </MapboxGL.MapView>
+      </View>
     </View>
   );
 };
