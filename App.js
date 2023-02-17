ddd1394756d4db30b92f906800be0012a7163860
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-16 17:52:27 -0800

    Adds and tests ENV's with react-native-dotenv lib

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React, {useState} from 'react';
 import {View, StyleSheet, Text, Image} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import {REACT_APP_TEST_ENV, REACT_APP_ACCESS_KEY} from '@env';
+
+// MapboxGL.setAccessToken(REACT_APP_ACCESS_KEY);
 
 const App = () => {
   const [coordinates] = useState([8.674252499999994, 9.0845755]);
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <MapboxGL.MapView style={styles.map}>
-          <MapboxGL.Camera zoomLevel={6} centerCoordinate={coordinates} />
-          <MapboxGL.PointAnnotation coordinate={coordinates} />
-        </MapboxGL.MapView>
-      </View>
+    // <View style={styles.page}>
+    //   <View style={styles.container}>
+    //     <MapboxGL.MapView style={styles.map}>
+    //       <MapboxGL.Camera zoomLevel={6} centerCoordinate={coordinates} />
+    //       <MapboxGL.PointAnnotation coordinate={coordinates} />
+    //     </MapboxGL.MapView>
+    //   </View>
+    // </View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Test ENV: {REACT_APP_TEST_ENV} </Text>
+      <Text>Test ENV: {REACT_APP_ACCESS_KEY} </Text>
     </View>
   );
 };
